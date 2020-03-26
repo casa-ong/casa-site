181e2f56dd2cbcbd92b5b578eb3dc49e399f05d4
vinesnts <v.santos0406@gmail.com>
2020-03-26 20:52:43 -0300

    Adicionados icones do font-awesome

--- casa-site/resources/views/layout/_includes/header.blade.php
+++ casa-site/resources/views/layout/_includes/header.blade.php
@@ -5,9 +5,9 @@
     <!-- Estilo da pagina -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     <!-- JavaScript da pagina -->
-    <script src="{{ asset('js/dropdownOnClick.js') }}" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/dropdown_on_click.js') }}" crossorigin="anonymous"></script>
     <!-- Icones do Font Awesome -->
-    <!--script src="https://kit.fontawesome.com/8eafe50798.js" crossorigin="anonymous"></script-->
+    <script src="https://kit.fontawesome.com/8eafe50798.js" crossorigin="anonymous"></script>
     <!-- CSS for BootstrapCDN -->
     <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"-->
 </head>
