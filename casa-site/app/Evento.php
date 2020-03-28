a1566f735ebd8992648fb42c80b8b57be5fcbec8
raquel <raquellvieiraa@gmail.com>
2020-03-28 14:53:01 -0300

    correção do relacionamento entre entidade evento e usuário

--- casa-site/app/Evento.php
+++ casa-site/app/Evento.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Evento extends Model
 {
     protected $fillable = [
-        'nome', 'descricao', 'anexo', 'data', 'publicado'
+        'nome', 'descricao', 'anexo', 'data', 'publicado', 'user_id'
     ];
 }
