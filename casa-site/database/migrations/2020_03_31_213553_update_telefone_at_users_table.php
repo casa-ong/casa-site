3e8f62c3ade9b7d924078044c9df6a60fd93b585
vinesnts <v.santos0406@gmail.com>
2020-03-31 19:45:00 -0300

    Validações dos campos de projeto e voluntario concluida

---
+++ casa-site/database/migrations/2020_03_31_213553_update_telefone_at_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTelefoneAtUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('telefone')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- casa-site/resources/views/admin/voluntarios/_form.blade.php
+++ casa-site/resources/views/admin/voluntarios/_form.blade.php
@@ -1,22 +1,30 @@
+@if ($errors->any())
+    <p class="error">Campos com * são obrigatórios!</p>
+@endif
 <div class="input-field">
-    <label for="nome">Nome</label>
-    <input type="text" name="name" value="{{ isset($registro->name) ? $registro->name : '' }}" placeholder="Digite aqui o nome do voluntario">
+    <label for="nome">Nome*</label>
+    <input class="{{ $errors->has('name') ? 'error' : '' }}" type="text" name="name" value="{{ isset($registro->name) ? $registro->name : old('name') }}" placeholder="Digite aqui seu nome">
+    <p class="error">{{ $errors->first('name') }}</p>
 </div>
 <div class="input-field">
-    <label for="email">CPF</label>
-    <input type="text" name="cpf" value="{{ isset($registro->cpf) ? $registro->cpf : '' }}" placeholder="Digite aqui o cpf do voluntario">
+    <label for="cpf">CPF*</label>
+    <input class="{{ $errors->has('cpf') ? 'error' : '' }}" type="text" name="cpf" value="{{ isset($registro->cpf) ? $registro->cpf : old('cpf') }}" placeholder="Digite aqui seu cpf">
+    <p class="error">{{ $errors->first('cpf') }}</p> 
 </div>
 <div class="input-field">
-    <label for="email">Profissão</label>
-    <input type="text" name="profissao" value="{{ isset($registro->profissao) ? $registro->profissao : '' }}" placeholder="Digite aqui a profissao do voluntario">
+    <label for="profissao">Profissão*</label>
+    <input class="{{ $errors->has('profissao') ? 'error' : '' }}" type="text" name="profissao" value="{{ isset($registro->profissao) ? $registro->profissao : old('profissao') }}" placeholder="Digite aqui sua profissão">
+    <p class="error">{{ $errors->first('profissao') }}</p>
 </div>
 <div class="input-field">
-    <label for="nome">Descrição</label>
-    <textarea type="text" name="descricao" placeholder="Descreva o voluntario">{{ isset($registro->descricao) ? $registro->descricao : '' }}</textarea>
+    <label for="descricao">Descrição*</label>
+    <textarea class="{{ $errors->has('descricao') ? 'error' : '' }}" type="text" name="descricao" placeholder="Descreva você mesmo">{{ isset($registro->descricao) ? $registro->descricao : old('descricao') }}</textarea>
+    <p class="error">{{ $errors->first('descricao') }}</p>
 </div>
 <div class="input-field">
     <label for="foto">Foto do voluntario</label>
-    <input type="file" name="foto">
+    <input class="{{ $errors->has('foto') ? 'error' : '' }}" type="file" name="foto">
+    <p class="error">{{ $errors->first('foto') }}</p>
 </div>
 @if(@isset($registro->foto))
     <div class="input-field">
@@ -24,18 +32,26 @@
     </div>    
 @endisset
 <div class="input-field">
-    <label for="email">Email</label>
-    <input type="text" name="email" value="{{ isset($registro->email) ? $registro->email : '' }}" placeholder="Digite aqui o email do voluntario">
+    <label for="email">Email*</label>
+    <input class="{{ $errors->has('email') ? 'error' : '' }}" type="text" name="email" value="{{ isset($registro->email) ? $registro->email : old('email') }}" placeholder="Digite aqui seu email">
+    <p class="error">{{ $errors->first('email') }}</p>
 </div>
 <div class="input-field">
-    <label for="email">Telefone/Celular</label>
-    <input type="text" name="telefone" value="{{ isset($registro->telefone) ? $registro->telefone : '' }}" placeholder="Digite aqui o telefone do voluntario">
+    <label for="telefone">Telefone/Celular</label>
+    <input class="{{ $errors->has('telefone') ? 'error' : '' }}" type="text" name="telefone" value="{{ isset($registro->telefone) ? $registro->telefone : old('telefone') }}" placeholder="Digite aqui seu telefone">
+    <p class="error">{{ $errors->first('telefone') }}</p>
 </div>
 
 @if(!Auth::guest()) <!-- Garantir por validation que essa parte do codigo nao seja enviada sem autenticação -->
 <div class="input-field">
-    <label for="password">Senha de acesso</label>
-    <input type="password" name="password" value="" placeholder="Digite aqui a senha do voluntario">
+    <label for="password">Senha de acesso*</label>
+    <input class="{{ $errors->has('password') ? 'error' : '' }}" type="password" name="password" value="" placeholder="Digite aqui sua nova senha de acesso">
+    <p class="error">{{ $errors->first('password') }}</p>
+</div>
+<div class="input-field">
+    <label for="password_confirmation">Confirmação de senha de acesso*</label>
+    <input class="{{ $errors->has('password_confirmation') ? 'error' : '' }}" type="password" name="password_confirmation" value="" placeholder="Digite a senha novamente para confirmar">
+    <p class="error">{{ $errors->first('password_confirmation') }}</p>
 </div>
 <label class="input-checkbox" for="aprovado">Aprovar esse voluntário da ONG?
     <input type="checkbox" name="aprovado" {{ isset($registro->aprovado) && $registro->aprovado == true ? 'checked' : ''}} value="true">
