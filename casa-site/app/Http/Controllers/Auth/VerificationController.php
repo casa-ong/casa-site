c36bd4adbb85c238d594489823f6502bb32c215f
vinesnts <v.santos0406@gmail.com>
2020-05-13 00:40:31 -0300

    Corrigida confirmação de email de voluntario

--- casa-site/app/Http/Controllers/Auth/VerificationController.php
+++ casa-site/app/Http/Controllers/Auth/VerificationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Auth\Events\Verified;
+use App\User;
 
 class VerificationController extends Controller
 {
@@ -35,8 +38,21 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    public function verify(Request $request)
+    {
+        $user = User::find($request->route('id'));
+
+        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
+            throw new AuthorizationException;
+        }
+
+        if ($user->markEmailAsVerified())
+            event(new Verified($user));
+
+        return redirect()->route('site.voluntarios')->with('success', 'Email verificado com sucesso!');
     }
 }
