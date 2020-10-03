fe37b9faf2bb3a97907a4bf8448c69afbd10dcba
vinesnts <v.santos0406@gmail.com>
2020-10-03 19:23:45 -0300

    Adicionados testes de integraçao de Enquete

--- casa-site/app/Http/Middleware/VerifyCsrfToken.php
+++ casa-site/app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,14 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, \Closure $next)
+    {
+        if(env('APP_ENV') !== 'testing')
+        {
+            return parent::handle($request, $next);
+        }
+
+        return $next($request);
+    }
 }
--- casa-site/tests/Feature/EnqueteTest.php
+++ casa-site/tests/Feature/EnqueteTest.php
@@ -3,21 +3,24 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Enquete;
 
 class EnqueteTest extends TestCase
 {
+    use RefreshDatabase;
 
-    public function inicializarArrayEnquete()
+    public function inicializarArrayEnquete($user_id)
     {
         $enquete = Enquete::factory()
-            ->hasOpcao(3)
-            ->make();
+            ->create();
 
         $dados = $enquete->toArray();
+        $dados['user_id'] = $user_id;
+
+        $dados['opcao'][0] = 'Opcao 1';
+        $dados['opcao'][1] = 'Opcao 2';
         
         return $dados;
     }
@@ -38,14 +41,14 @@ class EnqueteTest extends TestCase
      * Usuario não logado é redirecionado para tela de login
      * ao tentar acessar rota de adicionar nova enquete
      */
-    public function testVoluntarioNaoLogadoAcessaForm()
+    public function testUsuarioNaoLogadoAcessaFormCriar()
     {
         $response = $this
             ->get('admin/enquete/adicionar')
             ->assertStatus(302);
     }
 
-    public function testVoluntarioAdminLogadoAcessaForm()
+    public function testVoluntarioAdminLogadoAcessaFormCriar()
     {
         $voluntario = User::factory()->create([
             'admin' => true,
@@ -57,14 +60,117 @@ class EnqueteTest extends TestCase
             ->assertStatus(200);
     }
 
-    public function testVoluntarioNaoLogadoEnviaForm()
+    public function testUsuarioNaoLogadoEnviaFormCriar()
     {
-        $dados = $this->inicializarArrayEnquete();
+        $dados = $this->inicializarArrayEnquete('');
 
         $response = $this
             ->post('admin/enquete/salvar', $dados)
             ->assertStatus(302);
     }
 
+    /**
+     * Voluntario é redirecionado para tela de lista de enquetes
+     * ao criar uma nova enquete
+     */
+    public function testVoluntarioLogadoEnviaFormCriar()
+    {
+        $voluntario = User::factory()->create([
+            'admin' => true,
+            'aprovado' => true,
+        ]);
 
+        $dados = $this->inicializarArrayEnquete($voluntario->id);
+
+        $response = $this
+            ->actingAs($voluntario)
+            ->post('admin/enquete/salvar', $dados)
+            ->assertStatus(302)
+            ->assertRedirect('admin/enquetes')
+            ->assertSessionHas('success');
+    }
+
+    /**
+     * Voluntario é redirecionado para tela de lista de enquetes
+     * ao criar uma nova enquete
+     */
+    public function testVoluntarioLogadoEnviaFormCriarIncompleto()
+    {
+        $voluntario = User::factory()->create([
+            'admin' => true,
+            'aprovado' => true,
+        ]);
+
+        $dados = $this->inicializarArrayEnquete($voluntario->id);
+        $dados['opcao'][0] = '';
+
+        $response = $this
+            ->actingAs($voluntario)
+            ->post('admin/enquete/salvar', $dados)
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+    }
+
+    public function testUsuarioNaoLogadoEnviaFormAtualizar()
+    {
+        $dados = $this->inicializarArrayEnquete('');
+
+        $response = $this
+            ->get('admin/enquete/atualizar/'.$dados['id'])
+            ->assertStatus(302);
+    }
+
+    /**
+     * Voluntario é redirecionado para tela de lista de enquetes
+     * ao atualizar uma enquete
+     */
+    public function testVoluntarioLogadoEnviaFormAtualizar()
+    {
+        $voluntario = User::factory()->create([
+            'admin' => true,
+            'aprovado' => true,
+        ]);
+
+        $dados = $this->inicializarArrayEnquete($voluntario->id);
+
+        $response = $this
+            ->actingAs($voluntario)
+            ->get('admin/enquete/atualizar/'.$dados['id'])
+            ->assertStatus(302)
+            ->assertSessionHas('success');
+    }
+
+    /**
+     * Voluntario é redirecionado para tela de lista de enquetes
+     * ao criar uma nova enquete
+     */
+    public function testUsuarioNaoLogadoDeletar()
+    {
+        $dados = $this->inicializarArrayEnquete('');
+
+        $response = $this
+            ->get('admin/enquete/deletar/'.$dados['id'])
+            ->assertStatus(302);
+    }
+
+    /**
+     * Voluntario é redirecionado para tela de lista de enquetes
+     * ao criar uma nova enquete
+     */
+    public function testVoluntarioLogadoDeletar()
+    {
+        $voluntario = User::factory()->create([
+            'admin' => true,
+            'aprovado' => true,
+        ]);
+
+        $dados = $this->inicializarArrayEnquete($voluntario->id);
+
+        $response = $this
+            ->actingAs($voluntario)
+            ->get('admin/enquete/deletar/'.$dados['id'])
+            ->assertStatus(302)
+            ->assertSessionHas('success');
+    }
+    
 }
