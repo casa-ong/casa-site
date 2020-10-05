19a260ed202cc4da125a2130604d1269852d3095
vinesnts <v.santos0406@gmail.com>
2020-10-04 21:03:03 -0300

    Adicionado zoom nas imagens das opcoes de enquete

--- casa-site/resources/views/site/enquetes/_form.blade.php
+++ casa-site/resources/views/site/enquetes/_form.blade.php
@@ -5,7 +5,7 @@
         <label class="radio-option" for="{{ $opcao->id }}">
             <input {{ $registro->is_aberta ? '' : 'disabled' }} type="radio" name="opcao" id="{{ $opcao->id }}" value="{{ $opcao->id }}">
             {{ $opcao->nome }} @auth {{ '('.($opcao->votos ?? '0').' votos)' }} @endauth
-            <div width="100px" height="100%" style="background: url('{{ asset($opcao->foto) }}'); background-size: contain" alt=""></div>
+            <div class="{{ isset($opcao->foto) ? 'img-zoomable' : '' }}" width="100px" height="100%" style="background: url('{{ isset($opcao->foto) ? asset($opcao->foto) : '' }}'); background-size: contain" alt=""></div>
         </label>
     @endforeach
     @error('opcao')
@@ -14,6 +14,8 @@
         </span>
     @enderror
 </div>
+{{-- Overlay para mostrar imagens aumentadas --}}
+<div class="overlay"></div>
 
 @if(env('APP_ENV') == 'testing')
     <input type="text" name="g-recaptcha-response" value="1" style="visibility: hidden;">
@@ -29,3 +31,25 @@
 
     </div>
 @endif
+
+@section('scripts')
+
+    <script>
+        // Image to Lightbox Overlay 
+        let images = document.querySelectorAll(".img-zoomable");
+        let overlay = document.querySelector(".overlay");
+
+        for(let i = 0; i < images.length; i++) {
+            images[i].addEventListener("click", function() {
+                overlay.style.backgroundImage = images[i].style.backgroundImage;
+                overlay.classList.toggle("open");
+            });
+        }
+
+        overlay.addEventListener("click", function() {
+            overlay.classList.toggle("open"); 
+        });
+        
+    </script>
+
+@endsection
